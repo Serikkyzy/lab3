fd9d4de63a93b4b33e0b70bc19692a3e6445afc2
Serikkyzy <61342751+Serikkyzy@users.noreply.github.com>
2021-02-24 12:24:57 +0600

    Add files via upload

---
+++ lab3 web/src/app/products.ts
@@ -0,0 +1,125 @@
+export const products = [
+  {
+    id: 1,
+    name:
+      "Razer BlackWidow Mechanical Gaming Keyboard: Green Mechanical Switches - Tactile & Clicky - Chroma RGB Lighting - Anti-Ghosting - Programmable Macro Functionality",
+    description:
+      "Signature Mechanical Switch for Gaming: Razer Green switch technology provides a satisfying click sound with 50 G of actuation force, ideal for typing and gaming requiring the highest accuracy",
+    rating: 4.1,
+    amazonSrc:
+      "https://www.amazon.com/Razer-BlackWidow-Mechanical-Gaming-Keyboard/dp/B07NZX84HB/ref=sr_1_7?dchild=1&field-shipping_option-bin=3242350011&pf_rd_i=16225009011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=85a9188d-dbd5-424e-9512-339a1227d37c&pf_rd_r=7XFXPWP06N3XJNQP899W&pf_rd_s=merchandised-search-5&pf_rd_t=101&qid=1588978526&rnid=1266092011&s=electronics&sr=1-7",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/71sXzUqI9XL._AC_SL1500_.jpg"
+  },
+  {
+    id: 2,
+    name:
+      "DEGOL Skipping Rope with Ball Bearings Rapid Speed Jump Rope Cable and 6” Memory Foam Handles Ideal for Aerobic Exercise Like Speed Training, Extreme Jumping, Endurance Training and Fitness Gym",
+    description:
+      "Smooth and Fast: the ball bearing system avoids the twisting, winding or bending like other fitness ropes, it ensures stable and relaxed rotation, as our skipping rope can bear heavy load, which brings your a perfect exercise of skipping rope, also provides the best fluency for elite fitness professionals.",
+    rating: 3.2,
+    amazonSrc:
+      "https://www.amazon.com/DEGOL-Skipping-Tangle-Free-Bearings-Endurance/dp/B07P2F2YHT/ref=lp_16225014011_1_5",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/51YYXSJ4T3L._AC_SL1001_.jpg"
+  },
+  {
+    id: 3,
+    name: "100PCS 3 ply black disposable face shield filter protection",
+    description:
+      "1.3 Layers of Material-It's made of 3 layers of high-quality non-woven fabric; it has a stronger filtering effect and is more warm and breathable.",
+    rating: 3.4,
+    amazonSrc:
+      "https://www.amazon.com/100PCS-disposable-shield-filter-protection/dp/B08C7HDF1F/ref=lp_16225006011_1_7",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/617D0j72B1L._SL1000_.jpg"
+  },
+  {
+    id: 4,
+    name:
+      "HP Chromebook 14-inch HD Laptop, Intel Celeron N4000, 4 GB RAM, 32 GB eMMC, Chrome (14a-na0010nr, Mineral Silver)",
+    description:
+      "Google Play Store: The millions of Android apps you know and love on your phone and tablet can now run on your Chrome device without compromising their speed, simplicity or security",
+    rating: 3.7,
+    amazonSrc:
+      "https://www.amazon.com/HP-Chromebook-14-inch-Celeron-14a-na0010nr/dp/B08529TZMC/ref=sr_1_1?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=0TJJA6PJAHH01QHW179V&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614099537&sprefix=laptops%2Caps%2C236&sr=8-1",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/91shKLxoedL._AC_SL1500_.jpg"
+  },
+  {
+    id: 5,
+    name:
+      "HP Stream 14 HD Laptop , Intel Celeron N4000 , 4GB RAM, 64GB eMMC, HDMI, Webcam, WiFi, Bluetooth, Windows 10 S",
+    description:
+      "Intel Celeron N4000 (1.1 GHz base frequency, up to 2.6 GHz 4 GB DDR4-2400 SDRAM (1 x 4 GB). 64 GB eMMC storage. Integrated Intel UHD Graphics 600; 1 multi-format SD media card reader; No optical drive.",
+    rating: 4.6,
+    amazonSrc:
+      "https://www.amazon.com/HP-Stream-Celeron-Bluetooth-Windows/dp/B08M8P554T/ref=sr_1_4?crid=2GD9704ZSJ6XU&dchild=1&keywords=laptops&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=516a7b4e-d342-442c-96c9-4fbeb114cb90&pf_rd_r=0TJJA6PJAHH01QHW179V&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614099572&sprefix=laptops%2Caps%2C236&sr=8-4",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/71SR3JaVu9L._AC_SL1500_.jpg"
+  },
+  {
+    id: 6,
+    name:
+      "Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black",
+    description:
+      "And Radeon free sync technology. No compatibility for VESA Mount",
+    rating: 4.9,
+    amazonSrc:
+      "https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg"
+  },
+  {
+    id: 7,
+    name:
+      "AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler",
+    description:
+      "The world's most advanced processor in the desktop PC gaming segment",
+    rating: 5.0,
+    amazonSrc:
+      "https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=lp_16225007011_1_12",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/71WPGXQLcLL._AC_SL1384_.jpg"
+  },
+  {
+    id: 8,
+    name:
+      "Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 256 GB",
+    description:
+      "Next-level Hardware - Make every move count with a blazing-fast processor and our highest-resolution display",
+    rating: 2.4,
+    amazonSrc:
+      "https://www.amazon.com/Oculus-Quest-Advanced-All-One-2/dp/B08F7PTF54/ref=sr_1_2?dchild=1&keywords=video+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=V8EHFE56QKYMB9726K8X&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614099727&sr=8-2",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/615YaAiA-ML._SL1500_.jpg"
+  },
+  {
+    id: 9,
+    name: "Scrabble Game",
+    description:
+      "Classic Scrabble game has the letter tiles and game guide for a wordy showdown",
+    rating: 3.6,
+    amazonSrc:
+      "https://www.amazon.com/Hasbro-Gaming-A8166-Scrabble-Game/dp/B00IL5XY9K/ref=sr_1_2?dchild=1&keywords=board+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=V8EHFE56QKYMB9726K8X&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614099767&sr=8-2",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/81OjLGNO5VL._AC_SL1500_.jpg"
+  },
+  {
+    id: 10,
+    name: "Sorry! Game",
+    description:
+      "Party it up, and surprise guests at your next event with laugh out loud game from Hasbro gaming",
+    rating: 4.0,
+    amazonSrc:
+      "https://www.amazon.com/Hasbro-Gaming-A5065-Sorry-Game/dp/B076HK9H7Z/ref=sr_1_3?dchild=1&keywords=board+games&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=V8EHFE56QKYMB9726K8X&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614099797&sr=8-3",
+    imageSrc:
+      "https://images-na.ssl-images-amazon.com/images/I/811mGsv%2BY8L._AC_SL1500_.jpg"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
